00f0ab8c851810e60992ff430d46ebaad5c9e2b5
crystalhe97 <yuweihe@uw.edu>
2024-03-12 23:57:06 -0700

    update

--- app.py
+++ app.py
@@ -3,27 +3,41 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import folium
+from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 import altair as alt
+from db import get_db_conn
 
 # Set the page title
 st.title('LinkedIn Job Listings')
 
-# Read the CSV file
-df = pd.read_csv('Enhanced_LinkedIn_Job_Listings.csv')
+# Function to execute read query and return result as DataFrame
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    result = cursor.fetchall()
+    columns = [i[0] for i in cursor.description]
+    return pd.DataFrame(result, columns=columns)
 
-# Ensure that Job_location is a string, and handle NaN values
-df['Job_location'] = df['Job_location'].fillna('Unknown').astype(str)
+# SQL query to select data from the table
+select_jobs_query = """
+SELECT * FROM newtable;
+"""
+
+# Create the database connection
+conn = get_db_conn()
+
+# Execute the query and convert to DataFrame
+df = execute_read_query(conn, select_jobs_query)
 
 # Rename columns for compatibility with st.map()
-df.rename(columns={'Latitude': 'lat', 'Longitude': 'lon'}, inplace=True)
-
-# Preparing the DataFrame for display
+df['Job_location'] = df['Job_location'].fillna('Unknown').astype(str)  
+df.rename(columns={'latitude': 'lat', 'longitude': 'lon'}, inplace=True)  
 df['Job_description_display'] = df['Job_description'].apply(lambda x: (x[:50] + '...' if len(x) > 50 else x) if isinstance(x, str) else x)
 
 # Getting unique values for filtering
 seniority_levels = df['Seniority_level'].unique().tolist()
-job_locations = sorted(list(set(df['Job_location']) - {'Unknown'}))
+job_locations = sorted(list(set(df['Job_location']) - {'Unknown', 'United States'}))
 
 # Filters
 selected_seniority_levels = st.multiselect("Filter by Seniority Level", options=seniority_levels, default=seniority_levels)
@@ -35,40 +49,33 @@ if selected_job_location != 'All':
     df_filtered = df_filtered[df_filtered['Job_location'] == selected_job_location]
 
 # Filter for map data to include only rows with latitude and longitude
-df_map_data = df_filtered.dropna(subset=['lat', 'lon'])
+df_filtered = df_filtered.drop(columns=['Location'], errors='ignore')
 
-# Display filtered DataFrame
+# 显示过滤后的DataFrame
 if not df_filtered.empty:
+    # 隐藏不需要的列
     columns_to_hide = ['Keyword', 'Person_hiring']
     df_display = df_filtered.drop(columns=columns_to_hide, errors='ignore')
     st.dataframe(df_display)
 
-    # Display full Job Description
-    job_descriptions = df_filtered['Job_description'].tolist()
-    selected_description = st.selectbox("View Full Job Description", job_descriptions)
-    st.text_area("Full Job Description", selected_description, height=300)
-
-    # Interactive bar chart
-    category = st.selectbox("Select a category", df['Seniority_level'].unique())
-    chart_data = df[df['Seniority_level'] == category]
+    # 交互式条形图
+    category = st.selectbox("Select a category", seniority_levels)  
+    chart_data = df_filtered[(df_filtered['Seniority_level'] == category) & (df_filtered['Job_location'] != 'United States')]
     bar_chart = alt.Chart(chart_data).mark_bar().encode(
         x=alt.X("count()", title="Number of Listings"),
-        y=alt.Y("Job_location", sort='-x', title="Job Location")
+        y=alt.Y("Job_location", sort='-x', title="Job Location"),
+        color=alt.Color('Job_location', legend=alt.Legend(title="Locations"))
     ).properties(
         title=f"Distribution of Job Listings in {category}"
     ).interactive()
     st.altair_chart(bar_chart, use_container_width=True)
 
-    # Map visualization using Folium
-    # Display the map if we have map data
+    # 使用Folium的热力图显示地图
+    df_map_data = df_filtered.dropna(subset=['lat', 'lon'])  # 确保仅对有地理信息的行进行处理
     if not df_map_data.empty:
         m = folium.Map(location=[df_map_data['lat'].mean(), df_map_data['lon'].mean()], zoom_start=4)
-        for idx, row in df_map_data.iterrows():
-            folium.Marker(
-                location=[row['lat'], row['lon']],
-                popup=row['Job_location']
-            ).add_to(m)
-        st_folium(m, width=725, height=500)
-
+        heat_data = [[row['lat'], row['lon']] for index, row in df_map_data.iterrows()]
+        HeatMap(heat_data).add_to(m)
+        st_folium(m, width=1800, height=600)
 else:
     st.write("No job listings match your filters.")
--- db.py
+++ db.py
@@ -1,22 +1,39 @@
 import os
-import psycopg2
+import mysql.connector
 from dotenv import load_dotenv
 
 # Load environment variables from a .env file
 load_dotenv()
 
-# Retrieve environment variables using appropriate keys and set default values
+# Retrieve environment variables using appropriate keys
 db_user = os.getenv('DB_USER')
 db_pw = os.getenv('DB_PASSWORD')
 db_host = os.getenv('DB_HOST')
 db_port = os.getenv('DB_PORT')
 db_name = os.getenv('DB_NAME')
 
-# Construct the connection string
-conn_str = f'postgresql://{db_user}:{db_pw}@{db_host}:{db_port}/{db_name}'
-
 def get_db_conn():
-    # Establish and return a database connection
-    conn = psycopg2.connect(conn_str)
-    conn.autocommit = True
+    # Construct the connection string
+    conn = mysql.connector.connect(
+        user=db_user,
+        password=db_pw,
+        host=db_host,
+        port=db_port,
+        database=db_name,
+        ssl_disabled=True
+    )
     return conn
+
+
+if __name__ == "__main__":
+    print(db_user)
+    print(db_name)
+    print(db_port)
+    print(db_pw)
+    print(db_host)
+    conn = get_db_conn()
+    if conn.is_connected():
+        print("Successfully connected to the database")
+        conn.close()
+    else:
+        print("Database connection failed")
